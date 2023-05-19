77d3eb414e31ea5f2f4c40251f5a156356037595
Manuel Hinke <vigo@DESKTOP-7P4JOVB>
2023-05-19 12:48:48 +0200

    Inital react setup and prerequisites

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import king_vigo from './assets/images/king_vigo.png';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="header">
+      <header>
+        {/* Header content goes here */}
+      </header>
+      <main className='main'>
+        <div className="overlay">
+          <div className="container">
+            <img src={king_vigo} alt="Count Vigo" className="mint_vigo_img"/>
+            <button className="mint-button">Connect Wallet</button>
+          </div>
+        </div>
+      </main>
+    </div>  
+  );
+};
+
+export default App;
